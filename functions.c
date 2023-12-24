0a75a96346a85aa08aca040c7f86303d5df7a112
MichaelSuthirta <153809592+MichaelSuthirta@users.noreply.github.com>
2023-12-24 14:23:00 +0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -12,6 +12,196 @@ int menu(){
 	printf("Insert your choice: ");
 	scanf("%d",&choice);
 	return choice;
+}#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+struct gameData{
+	int run[1000];
+	int score[1000];
+};
+
+int checkName(){
+	FILE* fp=fopen("user.txt","r");
+	fseek(fp,0,SEEK_END);
+	int size=ftell(fp);
+	if(size==0){
+		return 0;
+	}
+	else return 1;
+}
+
+void saveName(char name[100]){
+	FILE* fp=fopen("user.txt","w");
+	fprintf(fp,"%s",name);
+	fclose(fp);
+}
+
+int menu(){
+	int choice;
+	printf("  1. Play the game			\n");
+	printf("  2. View Highscore			\n");
+	printf("  3. How to Play?   		\n");
+	printf("  4. Change Username			\n");
+	printf("  5. Exit the game			\n");
+	printf("Insert your choice: ");
+	scanf("%d",&choice);
+	return choice;
+}
+
+int readScore(){
+    FILE *fscore = fopen("score.txt", "r");
+    int score=1;
+    int highScore=0;
+    while(!feof(fscore)){	
+  	  fscanf(fscore, "%d", &score);
+  	  if(score>highScore){
+			highScore=score;
+		}
+	}
+    fclose(fscore);
+    return highScore;
+}
+
+void saveScore(int score){
+    FILE *fscore = fopen("score.txt", "a+");
+    fprintf(fscore, "%d\n", score);
+    fclose(fscore);
+    printf("%d", score);
+}
+
+void tutorial(){
+	printf("\tWelcome to The Gold Mine. In this game, you'll work as a miner in an abandoned gold mine that has just been reopened. ");
+	printf("Your task is to gather as many gold ores as possible."
+	" However, there are bombs in the mine, left by the previous workers in the mine. ");
+	printf("You must avoid the bombs. However, you aren't provided with safety measures and can only rely on leftover safety kit."
+	" You can dig at ");
+	printf("any coordinate among the provided 8x8 field. When digging, there's a chance one of the following items will appear:\n");
+	printf("1. Gold. It adds to your score.\n2. Bomb. It decreases your lifepoint.\n");
+	printf("3. Empty.\n4. Safety kit. It restores your lifepoint.\nEnjoy the game!");
+}
+
+void cls(){
+	system("cls");
+}
+
+void display(char map[8][8]){
+	int x,y;
+
+	for(x=0;x<8;x++){
+		for(y=0;y<8;y++){
+			printf("%c ",map[x][y]);
+		}
+		printf("\n");
+	}
+}
+
+void showLife(int *hp){
+	int i;
+	int x=*hp;
+	printf("Life: ");
+	for(i=1;i<=x;i++){
+		printf("o ");
+	}
+	printf("\n");
+}
+
+void showGold(int *gold){
+	printf("Gold: %d\n",*gold);
+}
+
+void game(char map[8][8]){
+	cls();
+	int hp=3, safe=2, gold=0;
+	int x,y;
+	int *life=&hp;
+	int *score=&gold;
+	int res=0;
+	int prevRes=0, goldCount=0, emptyCount=0;
+	while(hp!=0){
+		showLife(life);
+		showGold(score);
+		display(map);
+		printf("\nDigging coordinate (x y): (Coordinate starts from (1 1) to (8 8))\n");
+		scanf("%d %d",&x,&y);
+		if(x>8 || y>8){
+			printf("Please dig between the 8x8 field.\n");
+			sleep(1);
+			cls();
+			continue;
+		}
+		srand(time(0));
+		if(safe>0){
+			res=(rand()%4)+1;
+		}
+		else{
+			res=(rand()%(3))+1;
+		}
+		if(res==3 && res==prevRes){
+			res=(rand()%(3))+1;
+		}
+		if(goldCount==4 && res==1){
+			res+=((rand()%2)+1);
+			goldCount=0;
+		}
+		if(emptyCount==5 && res==2){
+			if(gold>8){
+				res++;
+			}
+			else if(gold<3){
+				res--;
+			}
+			else{
+				while(res==2){
+					res=(rand()%(3))+1;
+				}
+			}
+			emptyCount=0;
+		}
+		map[x-1][y-1]='X';
+		cls();
+		switch(res){
+			case 1:{
+				printf("Gold.\n");
+				gold++;
+				goldCount++;
+				break;
+			}
+			case 2:{
+				printf("Nothing here.\n");
+				emptyCount++;
+				break;
+			}
+			case 3:{
+				printf("A bomb!\n");
+				hp--;
+				prevRes=res;
+				break;
+			}
+			case 4:{
+				printf("You found a safety kit.\n");
+				hp++;
+				safe--;
+				break;
+			}
+			default:{
+				break;
+			}
+		}
+		sleep(1);
+		cls();
+	}
+	if(*life==0){
+		saveScore(gold);
+		char input;
+		cls();
+		printf("Game Over!\n");
+		printf("High Score: %d\n",gold);
+		printf("Press x to return.\n");
+		while(input!='x' && input !='X'){
+			scanf("%c",&input);
+		}
+	}
 }
 
 int readScore(){
